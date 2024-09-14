096da9d4874c313405b987df4371f0229f063b43
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-15 01:27:18 +0530

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:8005/';
+$config['base_url'] = 'http://local.rocket.co.in:8081/URBANCLAP-CLONE/';
 
 /*
 |--------------------------------------------------------------------------
